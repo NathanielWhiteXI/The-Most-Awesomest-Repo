34c82fcdaa5dc600551c8db3afa7bfe87b68d1ff
Nathaniel White <nathaniel.white3@ontariotechu.net>
2025-02-13 21:47:05 -0500

    Added Python Code

---
+++ brian's dead.py
@@ -0,0 +1,13 @@
+def brians_Dead():
+    return """
+
+yo dude, y’know they killed the uh, the dog on Family Guy
+THE DOG BRIAN? yeah, Brian. MAN STOP PLAYING FR. Nah man, I’m not playing. BRIAN’S DEAD…? Yeah.
+UUAUUUAUAAUAUAUAUAAAAAAGGGGHHHHH! SHOOOOOOOOOOOTTTT!!!
+
+*bangs table* BRIIIIIANNNNSSS DEEEAAAAAAD!
+
+AAAAAAGHHH- AGHHHHHHH- *goes silent* DAAAAAH!
+"""
+
+print(brians_Dead())
